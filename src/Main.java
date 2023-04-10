f866d2f9b8404d8967a55f86d7f1698fbd6c437a
Tess Gaston Løvgreen <tess@lovegreen.life>
2023-04-10 23:06:02 +0200

    game lifecycle explicit in main

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,9 @@ public class Main {
     public static void main(String[] args) {
         //todo: change to instantiation of Game class
         Game game = new Game(6);
-        game.setup();
+        game.playerSetup();
+        game.boardSetup();
+        game.endGame();
 
     }
 }
