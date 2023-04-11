c2e59f25a5a1e43710299252912930dc101533be
Nicklas W <Nicklas2002@hotmail.com>
2023-04-11 13:21:53 +0200

    CardDeck 1

---
+++ src/model/CardDeck.java
@@ -0,0 +1,16 @@
+package model;
+
+public class CardDeck {
+
+    private String[] carddata = new String[100];
+
+
+    public CardDeck(String[] carddata){
+
+        carddata[0] = "Hej";
+        carddata[1] = "Med";
+        carddata[2] = "Dig!";
+
+    }
+
+}
