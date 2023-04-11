6fd9ccafc60557a5819f005dbc889549103fcc67
badranyoussef <79192971+badranyoussef@users.noreply.github.com>
2023-04-11 13:19:44 +0200

    Create Field.java

---
+++ src/model/Field.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Field {
+
+    String label;
+    int ID;
+    int cost;
+    int income;
+    String option;
+
+
+    public Field(int ID, String label, int cost, int income){
+
+        this.ID = ID;
+        this.label = label;
+        this.cost = cost;
+        this.income = income;
+
+    }
+
+
+
+
+}
