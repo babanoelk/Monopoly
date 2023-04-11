20e1b3431c69a9da156c7ec8fb5d00653f6db3e7
Tess Gaston Løvgreen <tess@lovegreen.life>
2023-04-11 15:17:11 +0200

    added subsubclass

--- src/model/Plot.java
+++ src/model/Plot.java
@@ -1,8 +1,8 @@
 package model;
 
 public class Plot extends Property{
-    public Plot(int ID, String label, int cost, int income) {
-        super(ID, label, cost, income);
+    public Plot(int ID, String label, int cost, int income, int seriesID) {
+        super(ID, label, cost, income, seriesID);
     }
 
     public String onLand(Player p){
