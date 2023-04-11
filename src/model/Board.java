17fd6d61e47d38c0c6c059ab867e962b55962766
Tess Gaston Løvgreen <tess@lovegreen.life>
2023-04-11 15:10:37 +0200

    added get next card Trim on integer parsing Started switch case

--- src/model/Board.java
+++ src/model/Board.java
@@ -13,12 +13,39 @@ public class Board {
         this.cardDeck = new CardDeck(cards);
 
     }
+public String getNextCard(){
 
+      return cardDeck.getNext();
+}
     private void createFields(String[]data){
         int counter =0;
         for(String s : data){
             String[] line = s.split(",");
-            Field f = new Field(line[0],line[1],line[2],line[3],line[4]);
+            int id = Integer.parseInt(line[0].trim());
+            String type = line[1];
+            String label = line[2];
+            int cost = Integer.parseInt(line[3].trim());
+            int income =Integer.parseInt(line[4].trim());
+            int seriesID =Integer.parseInt(line[5].trim());
+
+
+            Field f;
+           // Field f = new Field(line[0],line[1],line[2],line[3],line[4]);
+
+            switch(type){
+                case "Plot": f = new Plot(id, label, cost, income, seriesID);
+                break;
+                case "Brewery": f = new Field(id, label, cost, income);
+                break;
+               case "ShippingLine": f = new Field(id, label, cost, income);
+                    break;
+                case "Tax": f = new Field(id, label, cost, income);
+                    break;
+                default: f = new Field(id, label, cost, income);
+
+
+            }
+
             fields[counter] = f;
             counter++;
         }
